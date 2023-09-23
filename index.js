28826cd2216f6431ba331b780a9aaef6b5913df0
abhisn15 <abhinugroho41@smp.belajar.id>
2023-09-23 12:26:20 +0700

    update menambahkan sebuah logika baru dan juga menambahkan tahun secara (REALTIME)

--- index.js
+++ index.js
@@ -16,21 +16,6 @@
 //     const peringatanView = document.getElementById("peringatan")
 //     peringatanView.style = 'color: red;'
 
-//     if (result <= 3 && result >= 1) {
-//         resultView.innerHTML += " dan golongan manusia (balita)";
-//     } else if (result <= 10 && result >= 3) {
-//         resultView.innerHTML += " dan golongan manusia (anak-anak)"
-//     } else if (result <= 18 && result >= 10) {
-//         resultView.innerHTML += " dan golongan manusia (remaja)"
-//     } else if (result <= 30 && result >= 18) {
-//         resultView.innerHTML += " dan golongan manusia (dewasa)"
-//     } else if (result <= 60 && result >= 30) {
-//         resultView.innerHTML += " dan golongan manusia (orang tua)"
-//     } else if (result <= 100 && result >= 60) {
-//         resultView.innerHTML += " dan golongan manusia (kakek/nenek)"
-//     } else if (result <= 9999 && result >= 100) {
-//         resultView.innerHTML += " dan golongan manusia (tertua didunia)"
-//     }
 
 //     if (inputUltahValue === '') {
 //         resultView.style = 'display: none;';
@@ -60,22 +45,39 @@ function validasiForm() {
     const inputUltahValue = inputUltah.value;
 
     // INI PERHITUNGAN UNTUK TAHUN SEKARANG SECARA REALTIME
-    const tahunSekarang = new Date().getFullYear();
+    const tahunSekarang = new Date().getFullYear()
 
     // INI ADALAH PERHITUNGAN TAHUN SEKARANG DIKURANGI ULANG TAHUN YANG DI INPUT OLEH USER
-    const hasil = tahunSekarang - inputUltahValue;
+    const hasil = tahunSekarang - inputUltahValue
 
     // INI UNTUK MEMEMANGGIL ID HASIL DI DALAM TAG <p> LALU MENAMBAHKAN VALUE DARI TAG <p> MENGGUNAKAN inner.HTML = 'value untuk tag <p>' + hasil dari pengurangan di atas
+    // INI ADALAH PENGGUNAAN CSS DI DALAM JAVASCRIPT
     const hasilView = document.getElementById('hasil')
-    hasilView.innerHTML = 'Umur anda sekarang adalah: ' + hasil + ' Tahun';
+    hasilView.innerHTML = 'Umur anda sekarang adalah: ' + hasil + ' Tahun'
 
     const peringatanView = document.getElementById('peringatan')
     peringatanView.innerHTML = 'Mohon isi tahun kelahiran anda'
 
-    // INI ADALAH PENGGUNAAN CSS DI DALAM JAVASCRIPT
+    // INI ADALAH LOGIKA UNTUK USER KETIKA MENGINPUT SETIAP HASIL UMUR ANDA SEKARANG ADALAH DITAMBAH DENGAN KONTEN TEXT TAMBAHAN ( .innerHTML += ) BERUPA GOLONGAN
+    if (hasil <= 3 && hasil >= 1) {
+        hasilView.innerHTML += " dan golongan manusia (balita)"
+    } else if (hasil <= 10 && hasil >= 3) {
+        hasilView.innerHTML += " dan golongan manusia (anak-anak)"
+    } else if (hasil <= 18 && hasil >= 10) {
+        hasilView.innerHTML += " dan golongan manusia (remaja)"
+    } else if (hasil <= 30 && hasil >= 18) {
+        hasilView.innerHTML += " dan golongan manusia (dewasa)"
+    } else if (hasil <= 60 && hasil >= 30) {
+        hasilView.innerHTML += " dan golongan manusia (orang tua)"
+    } else if (hasil <= 100 && hasil >= 60) {
+        hasilView.innerHTML += " dan golongan manusia (kakek/nenek)"
+    } else if (hasil <= 2024 && hasil >= 100) {
+        hasilView.innerHTML += " dan golongan manusia (kakek buyut/nenek buyut)"
+    } else {
+        hasilView.innerHTML += " dan golongan manusia (bayi)"
+    }
 
     // INI ADALAH LOGIC KETIKA USER MENGINPUT DENGAN NILAI YANG KOSONG
-
     if (inputUltahValue === '') {
         hasilView.style = 'display: none;'
         peringatanView.style = 'display: block; color: red;'
@@ -84,9 +86,10 @@ function validasiForm() {
         peringatanView.style = 'display: none;'
     }
 
-    if (inputUltahValue <= 9999 && inputUltahValue >= 2024) {
+    // ini adalah sebuah logika ketika user menginput sebuah angka dari inputUltahValue lebih dari tahun saat ini secara (REALTIME) dan mengubah konten dari peringatan view menjadi Tahun kelahiran tidak boleh lebih dari tahun saat ini
+    if (inputUltahValue > tahunSekarang) {
         hasilView.style = 'display: none;';
-        peringatanView.innerHTML = 'Tahun kelahiran tidak boleh lebih dari tahun saat ini (2023)'
+        peringatanView.innerHTML = 'Tahun kelahiran tidak boleh lebih dari tahun saat ini'
         peringatanView.style = 'display: block; color: red; width: 240px;'
     }
 }
